8198e667774501829dbc2d02162309a338e81504
SUN XIAODI <e1349508@u.nus.edu>
2025-05-05 15:37:53 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,7 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Community Bonding Chef',
+  title: 'Chat bot',
   description: 'dify chatbot',
   copyright: 'Dify',
   privacy_policy: '',
