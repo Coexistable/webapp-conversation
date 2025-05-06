6381c5a9ffe15685aa56b41bce60c1e46c95ecf0
SUN XIAODI <e1349508@u.nus.edu>
2025-05-06 13:29:06 +0800

    Update index.tsx

--- app/components/chat/index.tsx
+++ app/components/chat/index.tsx
@@ -145,7 +145,7 @@ const Chat: FC<IChatProps> = ({
       </div>
       {
         !isHideSendInput && (
-          <div className={cn(!feedbackDisabled && '!left-3.5 !right-3.5', 'sticky bottom-0 z-10 bg-transparent')}>
+          <div className={cn(!feedbackDisabled && '!left-3.5 !right-3.5', 'absolute z-10 bottom-0 left-0 right-0')}>
             <div className='p-[5.5px] max-h-[150px] bg-white border-[1.5px] border-gray-200 rounded-xl overflow-y-auto'>
               {
                 visionConfig?.enabled && (
